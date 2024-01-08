8d03b93c992a35fa1723da5d7fed83da50235053
Felix A. Taveras <felixataveras1@gmail.com>
2024-01-08 18:10:58 -0500

    completed lab1

---
+++ README.md
@@ -0,0 +1,10 @@
+# 01 - Class 01
+
+### Project: Snake-cafe
+
+### Author: Felix A. Taveras
+
+## How to initialize/run your application
+```
+python snakes_cafe.py
+```
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -5,12 +5,60 @@ print("**                            **")
 print("**Type 'quit' any time to quit**")
 
 appetizers = ["Wings", "Cookies", "Spring Rolls"]
-entrees = ["", "", "", "", ""]
+dessert = ["Ice Cream", "Cake", "Pie", "a 10"]
+drinks = ["Coffee", "Tea", "Unicorn Tears"]
+entrees = ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]
+devider = "---------"
 
-    
+menu = [item.lower() for item in appetizers + entrees + dessert + drinks]
+order_count = {}
 
-userOrder = input("What would you like to order?")
+print("\nAppetizers")
+print(devider)
+for item in appetizers:
+    print(item)
+
+print("\nEntrees")
+print(devider)
+
+for item in entrees:
+    print(item)
+
+print("\nDessert")
+print(devider)
+for item in dessert:
+    print(item)
+
+print("\nDrinks")
+print(devider)
+for item in drinks:
+    print(item)
+print()
 
 
+print("*********************************")
+print("**What would you like to order?**")
+print("*********************************")
 
-print(userOrder)
+
+while True:
+    userOrder = input(">").lower()
+    if userOrder.lower() == 'quit':
+        print("\nYour order summary: ")
+        for item, count in order_count.items():
+            print(f"**** {count} of {item} ****")
+        print("\nThank you for visiting, All this looks like a 10...")
+        break
+    if userOrder in menu:
+        if userOrder in order_count:
+            order_count[userOrder] += 1
+        else:
+            order_count[userOrder] = 1
+        if order_count[userOrder] >= 2:
+            print(str(order_count[userOrder]) + " orders of " + userOrder + " added to your meal")
+        else:
+            print(str(order_count[userOrder]) + " order of " + userOrder + " added to your meal")
+    else:
+        print("It is not that kind of place, please try again...")
+
+
