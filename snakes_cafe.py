c071aade1885605731e4fb7b4ccb78a52af50ed0
Felix A. Taveras <felixataveras1@gmail.com>
2024-01-08 14:52:43 -0500

    first commit

---
+++ snakes_cafe.py
@@ -0,0 +1,16 @@
+print("*********************************")
+print("** Welcome to the snakes Cafe **")
+print("** Please see out menu below  **")
+print("**                            **")
+print("**Type 'quit' any time to quit**")
+
+appetizers = ["Wings", "Cookies", "Spring Rolls"]
+entrees = ["", "", "", "", ""]
+
+    
+
+userOrder = input("What would you like to order?")
+
+
+
+print(userOrder)
